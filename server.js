7e4ffdba701d77d905db3996519cf8a92d9b251f
fxrcode <fxrcode@gmail.com>
2021-03-03 16:25:21 -0800

    U4L4: added FAVORITE_FOOD to travis env

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const favoritefood = process.env.FAVORITE_FOOD;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -5,7 +6,6 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    const favoritefood = process.env.FAVORITE_FOOD;
     console.log(`Microservices rock: ${favoritefood}`);
     await sleep(5000);
   }
